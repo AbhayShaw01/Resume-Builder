d6de977775db96a07f9c28bf6741e9359dfb7845
AbhayShaw01 <abhayshaw0301@gmail.com>
2024-12-12 13:46:42 +0530

    feat: theme toggle button

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -4,7 +4,12 @@ import Link from "next/link";
 import logo from "@/assets/logo.jpg";
 import { UserButton } from "@clerk/nextjs";
 import { CreditCard } from "lucide-react";
+import ThemeToggle from "@/components/ui/ThemeToggle";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
+
 export default function Navbar() {
+  const { theme } = useTheme();
   return (
     <header className="shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
@@ -20,24 +25,28 @@ export default function Navbar() {
             Resume Builder
           </span>
         </Link>
-        <UserButton
-          appearance={{
-            elements: {
-              avatarBox: {
-                width: 35,
-                height: 35,
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          <UserButton
+            appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
+              elements: {
+                avatarBox: {
+                  width: 35,
+                  height: 35,
+                },
               },
-            },
-          }}
-        >
-          <UserButton.MenuItems>
-            <UserButton.Link
-              label="Billing"
-              labelIcon={<CreditCard className="size-4" />}
-              href="/billing"
-            />
-          </UserButton.MenuItems>
-        </UserButton>
+            }}
+          >
+            <UserButton.MenuItems>
+              <UserButton.Link
+                label="Billing"
+                labelIcon={<CreditCard className="size-4" />}
+                href="/billing"
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </div>
       </div>
     </header>
   );
